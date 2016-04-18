3c533d440752e6b73f695bf3b2e9723f6b2866d8
LMZStone <t22960st@gmail.com>
2016-04-19 00:40:33 +0800

    new base file

---
+++ make.js
@@ -0,0 +1,7 @@
+
+var first = document.createElement("H1");
+var text = document.createTextNode("Jason is pretty awesome");
+first.appendChild(text);
+
+console.log(document.getElementsByName("aaa"));
+
