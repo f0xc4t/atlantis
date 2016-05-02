af04dc7d0a48bd97e4c1e7e72bf00766c875bbbb
LMZStone <t22960st@gmail.com>
2016-05-02 19:14:29 +0800

    fix

--- js/model.js
+++ js/model.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    window.WebSocket = window.WebSocket || window.MozWebSocket;
-    var ws = new window.WebSocket("ws://player.louistone.com:8845");
+    var ws = new window.WebSocket("ws://localhost:8843");
     console.log(ws);
     $('.msg').html(456); 
     
@@ -8,9 +8,14 @@ $(document).ready(function() {
     ws.onmessage = function(e) {
         $('.msg').append($("<div></div>").html(e.data));
         var _d = JSON.parse(e.data);
-        chrome.storage.local.set({"token": _d["token"]}, function() {
-            $('.msg').append($("<div></div>").html("save success"));
+        chrome.storage.local.get("token", function(data) {
+            if (!("token" in data)) {
+                chrome.storage.local.set({"token": _d["token"]}, function() {
+                    $('.msg').append($("<div></div>").html("save success"));
+                });
+            }
         });
+        
     };
         
     ws.onopen = function(e) {
@@ -22,10 +27,11 @@ $(document).ready(function() {
             if ("token" in data) {
                 console.log(data["token"]);
                 $('.msg').append($("<div></div>").html("token => " + data["token"]));
+                ws.send('{"login":{"token":"'+data["token"]+'"}}');
             } else {
                 var name = $('.name').val();
                 if (name) {
-                    ws.send('{"login":{"name":'+name+'}}');
+                    ws.send('{"register":{"name":'+name+'}}');
                 }
                 
                 $('.name').value = "";
